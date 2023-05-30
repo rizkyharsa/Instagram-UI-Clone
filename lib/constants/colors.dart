4fb3701c121022eeb1e6d3ca317897d43dcf53b0
rizkyharsa <rizkyharsa2@gmail.com>
2023-05-30 20:22:51 +0700

    update search

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -7,6 +7,7 @@ const white = Color(0xFFFFFFFF);
 const black = Color(0xFF000000);
 const textFieldBackground = Color(0xFF262626);
 const buttonFollowColor = Color(0xFF0494F5);
+const grey = Color(0xFF767680);
 const storyBorderColor = [
     Color(0xFF9B2282),
     Color(0xFFEEA863)
--- lib/modules/search/search_screen.dart
+++ lib/modules/search/search_screen.dart
@@ -1,10 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class SearchScreen extends StatelessWidget {
   const SearchScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              padding: const EdgeInsets.only(left: 12, right: 12, top: 8, bottom: 8),
+              margin: const EdgeInsets.only(left: 8, right: 8, top: 6, bottom: 6),
+              decoration: BoxDecoration(borderRadius: BorderRadius.circular(12), color: Colors.grey.shade200),
+              child: Row(
+                children: [
+                  SvgPicture.asset("assets/icons/search_inactive.svg", width: 15, height: 15),
+                  const SizedBox(width: 12),
+                  const Text(
+                    "Search",
+                    style: TextStyle(fontFamily: 'SfProText', color: Colors.grey),
+                  ),
+                ],
+              ),
+            ),
+            GridView.builder(
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3,
+              ),
+              itemBuilder: (context, index) { 
+                return Container();
+              },
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
